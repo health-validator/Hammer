844ca3acc06fd3aa9657f8cc82e4cb03179627a6
Vadim Peretokin <vperetokin@gmail.com>
2019-06-06 22:19:48 +0200

    Use Plain style

--- Program.cs
+++ Program.cs
@@ -618,7 +618,7 @@ class Program
   {
     RuntimeManager.DiscoverOrDownloadSuitableQtRuntime();
 
-    QQuickStyle.SetStyle("Universal");
+    QQuickStyle.SetStyle("Default");
     QGuiApplication.SetAttribute(Qml.Net.ApplicationAttribute.EnableHighDpiScaling, true);
 
     using (var app = new QGuiApplication(args))
