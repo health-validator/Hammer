1e218d5bba9d9da7daea228cb45b2ea9a931a4c6
Vadim Peretokin <vperetokin@gmail.com>
2019-06-26 08:16:08 +0200

    Try tar to untar with

--- .CI/install-dotnet-macos.sh
+++ .CI/install-dotnet-macos.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+brew install tree
+
+tree .
+
 wget --quiet --output-file="dotnet-sdk.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/079e5b24-187f-4087-9a54-18285382f3e6/2ae97c55aae5e2c9472555839a48a83c/dotnet-sdk-3.0.100-preview6-012264-osx-x64.tar.gz"
 
-gunzip -c "dotnet-sdk.tar.gz" | tar xopf -
+tree .
 
-brew install tree
+tar --extract --file "dotnet-sdk.tar.gz"
 
 tree .
