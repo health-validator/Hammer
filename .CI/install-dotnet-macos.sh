d9fffb0d06b319285f28d5340cd46a65029a3fcd
Vadim Peretokin <vperetokin@gmail.com>
2019-06-26 08:29:12 +0200

    Use curl to download instead

--- .CI/install-dotnet-macos.sh
+++ .CI/install-dotnet-macos.sh
@@ -4,10 +4,10 @@ brew install tree
 
 tree .
 
-wget --quiet --output-file="dotnet-sdk.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/079e5b24-187f-4087-9a54-18285382f3e6/2ae97c55aae5e2c9472555839a48a83c/dotnet-sdk-3.0.100-preview6-012264-osx-x64.tar.gz"
+curl --silent --output "dotnet-sdk.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/079e5b24-187f-4087-9a54-18285382f3e6/2ae97c55aae5e2c9472555839a48a83c/dotnet-sdk-3.0.100-preview6-012264-osx-x64.tar.gz"
 
 tree .
 
-tar --extract --file "dotnet-sdk.tar.gz"
+tar --extract --file "dotnet-sdk.tar.gz" --directory ".dotnetsdk"
 
 tree .
