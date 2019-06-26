e77eafbd56325b4ffd4d2dc9c9265998debdda04
Vadim Peretokin <vperetokin@gmail.com>
2019-06-26 09:06:04 +0200

    Set path

--- .CI/install-dotnet-macos.sh
+++ .CI/install-dotnet-macos.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-brew install tree
-
-tree .
-
 curl --silent --output "dotnet-sdk.tar.gz" "https://download.visualstudio.microsoft.com/download/pr/079e5b24-187f-4087-9a54-18285382f3e6/2ae97c55aae5e2c9472555839a48a83c/dotnet-sdk-3.0.100-preview6-012264-osx-x64.tar.gz"
-
-tree .
 
 mkdir ".dotnetsdk"
 
 tar --extract --file "dotnet-sdk.tar.gz" --directory ".dotnetsdk"
 
-tree .
+export PATH=$PATH;"$(pwd)/.dotnetsdk"
