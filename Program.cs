37de22cbf1a0abf6458fe6ce3c34c402ae029525
Vadim Peretokin <vperetokin@gmail.com>
2021-05-08 08:07:53 +0200

    Fix warning with MAX_PATH

--- Program.cs
+++ Program.cs
@@ -694,7 +694,7 @@ class Program {
         public static class Extensions {
             [System.Runtime.InteropServices.DllImport("kernel32.dll")]
             static extern uint GetModuleFileName(IntPtr hModule, System.Text.StringBuilder lpFilename, int nSize);
-            static readonly int MAX_PATH = 255;
+            const int MAX_PATH = 255;
             public static string GetExecutablePath() {
                 if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows)) {
                     var sb = new System.Text.StringBuilder(MAX_PATH);
