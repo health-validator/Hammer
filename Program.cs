b991d4dd506d6eb04e107648948d5b5c8f7149f4
Vadim Peretokin <vperetokin@gmail.com>
2021-05-17 18:45:27 +0200

    Add code for a window icon - requires updated qml.net

--- Program.cs
+++ Program.cs
@@ -1210,6 +1210,7 @@ class Program
         QCoreApplication.OrganizationDomain = "Hammer.mc";
         QCoreApplication.OrganizationName = "Hammer";
         engine.Load(Path.Combine(AppModel.Extensions.GetApplicationLocation(), "Main.qml"));
+        app.SetWindowIcon(Path.Combine(AppModel.Extensions.GetApplicationLocation(), "assets", "hammer-logo.png"));
 
         // Once the GUI is loaded, we can start working with the AppModel
         // instance.
